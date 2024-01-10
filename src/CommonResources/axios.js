dde4709885e352f3963d8294c9c635530759de44
Abuassefa7 <abuassefa19@gmail.com>
2024-01-10 17:03:19 -0600

    url updated

--- src/CommonResources/axios.js
+++ src/CommonResources/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-	baseURL: "http://localhost:7700/api",
+	baseURL: "https://abu-evangadiforum-backend.onrender.com/api",
+
+	// baseURL: "http://localhost:7700/api"
 });
 export default instance;
